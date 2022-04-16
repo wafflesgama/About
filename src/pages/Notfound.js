b392ced8dc5f3060e511adfbdebf462981018abf
Guilherme Gama <guilherme.s.gam@gmail.com>
2022-04-16 21:46:48 +0100

    404 page done

--- src/pages/Notfound.js
+++ src/pages/Notfound.js
@@ -1,10 +1,16 @@
 import React from 'react'
-
+import { useNavigate } from 'react-router-dom';
 function Notfound() {
+  let navigate=useNavigate();
   return (
-    <div className='notfound-container'>
-      <div className='notfound-back'></div>
-      <div className='notfound'></div>
+    <div className='notfound-cont-container '>
+      <div className='notfound-container'>
+        {/* <div className='notfound-back'></div> */}
+        <div className='notfound animate__animated animate__fadeInUp'></div>
+      </div>
+        <h1>404 - Page not Found</h1>
+        <p>The page you are looking for might have a different URL or has been removed</p>
+        <button onClick={()=> navigate('/About')}>Return to home</button>
     </div>
   )
 }
